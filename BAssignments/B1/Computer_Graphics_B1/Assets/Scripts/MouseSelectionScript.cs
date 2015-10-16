5cd3e7bbb1dea1d4f475ec3be2113ee70e23ccfa
Anish Goyal <anishgoyal@outlook.com>
2015-10-16 14:14:21 -0400

    Add mouse selection, include a small example scene which is to be replaced with the navigation scene

---
+++ BAssignments/B1/Computer_Graphics_B1/Assets/Scripts/MouseSelectionScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseSelectionScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = gameObject.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                Transform target = hit.transform;
+                GameObject targGameObject = target.gameObject;
+                if (targGameObject.CompareTag("Selectable")) {
+                    targGameObject.tag = "Selected";
+                    foreach (Transform child in target.transform)
+                    {
+                        if (child.CompareTag("SelectionIndicator"))
+                        {
+                            child.gameObject.SetActive(true);
+                            break;
+                        }
+                    }
+                }
+                else if (targGameObject.CompareTag("Selected"))
+                {
+                    targGameObject.tag = "Selectable";
+                    foreach (Transform child in target.transform)
+                    {
+                        if (child.CompareTag("SelectionIndicator"))
+                        {
+                            child.gameObject.SetActive(false);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+	}
+}
