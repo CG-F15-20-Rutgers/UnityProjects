ab77bb7820142037dc78b9986da57bea4e15c8c0
David Goldman <dallasftball@gmail.com>
2015-10-16 20:20:13 -0400

    Directory Script

---
+++ BAssignments/B1/Computer_Graphics_B1/Assets/Scripts/DirectorScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirectorScript : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    // To be called by the MouseSelectionScript.
+    public void DirectAgents(Vector3 target) {
+        GameObject[] selectedObjects = GameObject.FindGameObjectsWithTag("Selected");
+        foreach (GameObject gameObject in selectedObjects) {
+            NavMeshAgent meshAgent = gameObject.GetComponent<NavMeshAgent>();
+            meshAgent.SetDestination(target);
+        }
+    }
+}
