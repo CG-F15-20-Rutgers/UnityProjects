fa6fe27978392c7723283ca614f46f8a5349c68d
Anish Goyal <anishgoyal@outlook.com>
2015-10-18 18:06:06 -0400

    Adjust scene, add in prefabs for camera rigging

---
+++ BAssignments/B1/Computer_Graphics_B1/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float VerticalMovement = Input.GetAxis("Vertical");
+        float HorizontalMovement = Input.GetAxis("Horizontal");
+        if (VerticalMovement != 0.0f || HorizontalMovement != 0.0f)
+        {
+            Vector3 movement = new Vector3(HorizontalMovement, 0.0f, VerticalMovement);
+            gameObject.GetComponent<Rigidbody>().velocity = movement;
+        }
+	}
+}
