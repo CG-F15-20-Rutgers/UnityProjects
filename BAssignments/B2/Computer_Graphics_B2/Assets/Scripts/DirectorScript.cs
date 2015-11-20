cdab9dc4bfb42c08e9a59c981ef61bd734b65afc
DavidGoldman <dallasftball@gmail.com>
2015-11-20 18:41:19 -0500

    Copy b1 to form b2; add core

---
+++ BAssignments/B2/Computer_Graphics_B2/Assets/Scripts/DirectorScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirectorScript : MonoBehaviour {
+
+    private float agentSpeed;
+
+    // Called every frame
+    public void FixedUpdate()
+    {
+        float HorizontalObstacleMove = Input.GetAxis("ObstacleHorizontal");
+        float VerticalObstacleMove = Input.GetAxis("ObstacleVertical");
+        GameObject[] selectedObjects = GameObject.FindGameObjectsWithTag("ObstaclesToMove");
+        foreach (GameObject gameObject in selectedObjects) {
+            Vector3 movement = new Vector3(HorizontalObstacleMove, 0, VerticalObstacleMove);
+            // Adjust movement to be forward-facing
+            movement = transform.rotation * movement;
+            gameObject.GetComponent<Rigidbody>().freezeRotation = true;
+            gameObject.GetComponent<Rigidbody>().velocity = (movement * 10);
+        }
+    }
+
+    // To be called by the MouseSelectionScript.
+    public static void DirectAgents(Vector3 target, bool partThree, float speed) {
+        GameObject[] selectedObjects = GameObject.FindGameObjectsWithTag("Selected");
+        foreach (GameObject gameObject in selectedObjects) {
+            NavMeshAgent meshAgent = gameObject.GetComponent<NavMeshAgent>();
+            meshAgent.SetDestination(target);
+            if (partThree) meshAgent.speed = speed;
+        }
+    }
+}
---
+++ BAssignments/B2/Computer_Graphics_B2/Assets/Scripts/RobotController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotController : MonoBehaviour {
+
+    private NavMeshAgent agent;
+    private Animator animator;
+    private Rigidbody rb;
+
+    private float previousYRotation;
+    private bool isJumping;
+    private float time;
+    private Vector3 jumpStart;
+
+	// Use this for initialization
+	void Start () {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        animator = gameObject.GetComponent<Animator>();
+        rb = gameObject.GetComponent<Rigidbody>();
+        previousYRotation = transform.rotation.y;
+        isJumping = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float diffYRotation = transform.rotation.y - previousYRotation;
+        previousYRotation = transform.rotation.y;
+
+        animator.SetFloat("Speed", agent.velocity.magnitude);
+
+        if (agent.isOnOffMeshLink)
+        {
+            if (!isJumping)
+            {
+                animator.CrossFade(Animator.StringToHash("Base Layer.Jump Across"), 0.0f);
+                isJumping = true;
+                time = 0.0f;
+                jumpStart = transform.position;
+            }
+            else
+            {
+                time += Time.deltaTime;
+            }
+            float normal = time / 0.867f;
+            Vector3 pos = Vector3.Lerp(jumpStart, agent.currentOffMeshLinkData.endPos, normal);
+            transform.position = pos;
+            if (time >= 0.867f)
+            {
+                transform.position = agent.currentOffMeshLinkData.endPos;
+                agent.CompleteOffMeshLink();
+                agent.Resume();
+                isJumping = false;
+                animator.SetBool("NavMeshJump", false);
+            }
+        }
+	}
+}
