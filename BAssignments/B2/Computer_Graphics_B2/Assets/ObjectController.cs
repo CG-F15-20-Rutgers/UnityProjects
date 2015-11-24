5aefdfff7726fdcbc6ca42fcdf49db40e1a74af4
Anish Goyal <anishgoyal@outlook.com>
2015-11-24 02:22:23 -0500

    Add manipulation ability for player on egg

---
+++ BAssignments/B2/Computer_Graphics_B2/Assets/ObjectController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectController : MonoBehaviour {
+
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        foreach(Transform t in transform) {
+            if(t.name.Equals("egg")) {
+                rb = t.gameObject.GetComponent<Rigidbody>();
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float Horizontal = Input.GetAxis("Hor2");
+        float Vertical = Input.GetAxis("Ver2");
+        Vector3 velocity = new Vector3(Horizontal, 0, Vertical);
+        rb.velocity = velocity * 8f;
+	}
+}
