f8bdad61760e7ec78cdfb428fc277acec2523ec9
Anish Goyal <anishgoyal@outlook.com>
2015-11-24 02:36:10 -0500

    Add hover to egg

--- BAssignments/B2/Computer_Graphics_B2/Assets/ObjectController.cs
+++ BAssignments/B2/Computer_Graphics_B2/Assets/ObjectController.cs
@@ -3,22 +3,35 @@ using System.Collections;
 
 public class ObjectController : MonoBehaviour {
 
-    Rigidbody rb;
+    Rigidbody eggrb;
+    float currTime;
 
 	// Use this for initialization
 	void Start () {
         foreach(Transform t in transform) {
             if(t.name.Equals("egg")) {
-                rb = t.gameObject.GetComponent<Rigidbody>();
+                eggrb = t.gameObject.GetComponent<Rigidbody>();
             }
         }
+        currTime = 0;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+
+        // floating egg
+        Vector3 currPos = eggrb.position;
+        float y = currPos.y;
+        float newCurrTime = currTime + (Time.deltaTime) * 3f;
+        y += (Mathf.Sin(newCurrTime) - Mathf.Sin(currTime)) / 3f;
+        currPos.y = y;
+        eggrb.position = currPos;
+        currTime = newCurrTime;
+
+        // handle movement
         float Horizontal = Input.GetAxis("Hor2");
         float Vertical = Input.GetAxis("Ver2");
         Vector3 velocity = new Vector3(Horizontal, 0, Vertical);
-        rb.velocity = velocity * 8f;
+        eggrb.velocity = velocity * 8f;
 	}
 }
