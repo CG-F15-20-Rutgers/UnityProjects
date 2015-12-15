505ae0fc3cce039efe22ff872c843628dba61ac0
David Goldman <dallasftball@gmail.com>
2015-12-14 23:36:23 -0500

    Camera bounds for part 1

--- BAssignments/B3/Computer_Graphics_B3/Assets/Scripts/CameraP1Controller.cs
+++ BAssignments/B3/Computer_Graphics_B3/Assets/Scripts/CameraP1Controller.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class CameraP1Controller : MonoBehaviour {
 
+    private float minX = -64.0f;
+    private float maxX = 32.0f;
+    private float minZ = -76.0f;
+    private float maxZ = 20.0f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -17,6 +22,8 @@ public class CameraP1Controller : MonoBehaviour {
         rotationForward.x = 0;
         rotationForward.z = 0;
         Vector3 pos = transform.position + rotationForward * new Vector3(MoveX, 0, MoveZ);
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        pos.z = Mathf.Clamp(pos.z, minZ, maxZ);
         Quaternion rot = Quaternion.Euler(0, RotY, 0);
         transform.position = pos;
         transform.rotation = transform.rotation * rot;
