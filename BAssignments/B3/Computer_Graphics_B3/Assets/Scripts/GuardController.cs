a10f996d815b1959e419ede9f65120ba16e8a336
David Goldman <dallasftball@gmail.com>
2015-12-17 12:35:45 -0500

    More guard stuff

--- BAssignments/B3/Computer_Graphics_B3/Assets/Scripts/GuardController.cs
+++ BAssignments/B3/Computer_Graphics_B3/Assets/Scripts/GuardController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using TreeSharpPlus;
 
 public class GuardController : MonoBehaviour {
 
@@ -14,9 +15,40 @@ public class GuardController : MonoBehaviour {
         bool isPossessed = IsPossessed();
         if (isPossessed != wasPossessed)
         {
-            hint.gameObject.SetActive(isPossessed && dismissableNPC != null);
+            hint.gameObject.SetActive(isPossessed && CanDismissNPC());
+        }
+        if (isPossessed)
+        {
+            HandleInput();
         }
         wasPossessed = isPossessed;
+    }
+
+    BehaviorMecanim mec(GameObject g)
+    {
+        return g.GetComponent<BehaviorMecanim>();
+    }
+
+    void HandleInput()
+    {
+        // Control code copied from BehaviorTree3.cs@PossessedShopArc(GameObject).
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+        Vector3 target = transform.position + v * (Quaternion.Euler(0, h * 20, 0) * transform.forward);
+        Vector3 targetDirection = transform.position + 10 * (Quaternion.Euler(0, h * 40, 0) * transform.forward);
+        if (mec(gameObject).Character.NavGoTo(target) == RunStatus.Failure)
+            mec(gameObject).Character.NavStop();
+        if (mec(gameObject).Character.NavTurn(targetDirection) == RunStatus.Failure) ;
+             mec(gameObject).Character.NavOrientBehavior(OrientationBehavior.LookForward);
+
+        if (Input.GetKeyDown(KeyCode.Space) && hint.gameObject.activeSelf && dismissableNPC != null)
+        {
+            GetComponent<SpeechBubbleController>().DisplaySpeechBubble("Sir, I'm going to have to ask you to leave.");
+            dismissableNPC.GetComponent<PossessionScript>().IsDismissed = true;
+
+            // Hide the Hint b/c we just dismissed him.
+            hint.gameObject.SetActive(false);
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -25,7 +57,7 @@ public class GuardController : MonoBehaviour {
         {
             dismissableNPC = other.gameObject;
             wasPossessed = IsPossessed();
-            hint.gameObject.SetActive(wasPossessed);
+            hint.gameObject.SetActive(wasPossessed && CanDismissNPC());
         }
     }
 
@@ -36,6 +68,11 @@ public class GuardController : MonoBehaviour {
             dismissableNPC = null;
             hint.gameObject.SetActive(false);
         }
+    }
+
+    bool CanDismissNPC()
+    {
+        return (dismissableNPC != null && !dismissableNPC.GetComponent<PossessionScript>().IsDismissed);
     }
 
     bool IsPossessed()
