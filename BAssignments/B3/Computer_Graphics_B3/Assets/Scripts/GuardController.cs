7d9c4fcb4a7bb04b074e1c9acc412ed037c54c5a
David Goldman <dallasftball@gmail.com>
2015-12-17 12:07:24 -0500

    GuardController

---
+++ BAssignments/B3/Computer_Graphics_B3/Assets/Scripts/GuardController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuardController : MonoBehaviour {
+
+    [HideInInspector]
+    public GameObject dismissableNPC;
+    public UnityEngine.UI.Text hint;
+
+    private bool wasPossessed;
+
+    void Update()
+    {
+        bool isPossessed = IsPossessed();
+        if (isPossessed != wasPossessed)
+        {
+            hint.gameObject.SetActive(isPossessed && dismissableNPC != null);
+        }
+        wasPossessed = isPossessed;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Shopper") || other.gameObject.CompareTag("Thief"))
+        {
+            dismissableNPC = other.gameObject;
+            wasPossessed = IsPossessed();
+            hint.gameObject.SetActive(wasPossessed);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.Equals(dismissableNPC))
+        {
+            dismissableNPC = null;
+            hint.gameObject.SetActive(false);
+        }
+    }
+
+    bool IsPossessed()
+    {
+        PossessionScript ps = GetComponent<PossessionScript>();
+        return (ps != null && ps.IsPossessed);
+    }
+}
